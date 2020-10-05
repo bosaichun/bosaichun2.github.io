660228d88a6a96d859ebdc0794f2ca527fb8f8b3
bosaichun <72337854+bosaichun@users.noreply.github.com>
2020-10-05 20:31:41 +0800

    Add files via upload

---
+++ lmr.js
@@ -0,0 +1,8 @@
+alert("想进入李敏瑞的世界，请先回答我的问题：17679388+6736483=?");
+var result, value1,value2;
+value1=17679388
+value2=6736483
+result=value1+value2
+alert("太巨了！结果就是："+result)
+alert("欢迎聪明的你进入李敏瑞的世界！")
+alert("在左侧栏目中，我的网页还提供打印服务哟！");
